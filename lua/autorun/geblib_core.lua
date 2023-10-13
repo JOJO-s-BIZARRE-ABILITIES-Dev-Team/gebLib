d7535e8ce896d51e91485e52bf261da10b9bf903
TomsBack <oplpet95@gmail.com>
2023-10-13 13:10:48 +0200

    todo

--- lua/autorun/geblib_core.lua
+++ lua/autorun/geblib_core.lua
@@ -6,6 +6,7 @@ gebLib.Version = "0.0.0"
 gebLib.Build   = "0"
 //
 --------------------------
+--TODO: make this work with varargs
 function gebLib.PrintDebug( string ) -- Equivalent to print(), however prints only if gebLib_developer_debugmode is on 
     if !gebLib.DebugMode() then return end
     string = tostring(string)
